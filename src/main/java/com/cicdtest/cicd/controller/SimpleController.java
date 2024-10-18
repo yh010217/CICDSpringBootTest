89fcb17de56059986fe3abb60a96050d8f72a038
choi <cyh010217@gmail.com>
2024-10-18 17:26:32 +0900

    깃 클론을 안한 상태에서 시작하니까 뭔가 안가져오는 것도 있네

--- src/main/java/com/cicdtest/cicd/controller/SimpleController.java
+++ src/main/java/com/cicdtest/cicd/controller/SimpleController.java
@@ -9,4 +9,8 @@ public class SimpleController {
     public String home(){
         return "Hello, World CICD fourth";
     }
+    @GetMapping("hello")
+    public String hello(){
+        return "hello method added";
+    }
 }
