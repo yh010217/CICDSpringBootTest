e505a3f98d9b1acbbf13d56247123ecb0a18a2c5
choi <cyh010217@gmail.com>
2024-10-22 13:02:48 +0900

    nohup 을 nuhup이라 적음... 후...

--- scripts/start-server.sh
+++ scripts/start-server.sh
@@ -5,6 +5,6 @@ cd /home/ubuntu/CICDSpringBootTest
 sudo fuser -k -n tcp 80 ||true
 sudo fuser -k -n tcp 8080 ||true
 
-sudo nuhup java -jar project.jar > ./output.log 2>&1 &
+sudo nohup java -jar project.jar > ./output.log 2>&1 &
 echo "------서버 배포 끝--------"
 
