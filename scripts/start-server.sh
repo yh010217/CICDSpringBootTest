41fcc6972a22c8fdb38ddf05c5a155946a095e3b
choi <cyh010217@gmail.com>
2024-10-28 12:49:50 +0900

    확장성 고려한 도커기반 첫 푸시

--- scripts/start-server.sh
+++ scripts/start-server.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
 
 echo "-------서버 시작----------"
-cd /home/ubuntu/CICDSpringBootTest
-sudo fuser -k -n tcp 80 ||true
-sudo fuser -k -n tcp 8080 ||true
 
-sudo nohup java -jar project.jar > ./output.log 2>&1 &
+docker stop cicd-server || true
+docker rm cicd-server || true
+docker pull 147997119644.dkr.ecr.ap-northeast-2.amazonaws.com/typehoon/cicd-test:latest
+sudo docker run -d --name cicd-server -p 80:80 147997119644.dkr.ecr.ap-northeast-2.amazonaws.com/typehoon/cicd-test:latest
+
 echo "------서버 배포 끝--------"
 
+
+
+#cd /home/ubuntu/CICDSpringBootTest
+#sudo fuser -k -n tcp 80 ||true
+#sudo fuser -k -n tcp 8080 ||true
+
+#sudo nohup java -jar project.jar > ./output.log 2>&1 &
